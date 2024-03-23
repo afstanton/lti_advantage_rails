6a0d7c06382223ebccfbb76d7ea9cf795f7e3152
Aaron F Stanton <afstanton@gmail.com>
2024-03-23 15:38:34 -0400

    Fake the version

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_23_193459) do
+ActiveRecord::Schema[5.0].define(version: 2024_03_23_193459) do
   create_table "contexts", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
