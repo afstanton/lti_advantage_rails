8248669c8674cd02c5e66340c6fa6c5466d4da9e
Aaron F Stanton <afstanton@gmail.com>
2024-03-23 15:36:26 -0400

    See if having a table does it

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,5 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[5.0].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_23_193459) do
+  create_table "contexts", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
